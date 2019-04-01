37353c00c3411ef36ce1914e28845124beace196
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-04-01 10:52:29 -0700

    Add Cookies & CookieStore middleware to application.rb configurations

--- biddr_api/config/application.rb
+++ biddr_api/config/application.rb
@@ -43,5 +43,9 @@ module BiddrApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Enable middleware for cookies and sessions
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_biddr', expire_after: 30.days
   end
 end
