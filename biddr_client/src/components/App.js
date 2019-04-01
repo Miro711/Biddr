a771d29ced7a147dccde2d87523ab4f572f7e2ec
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-04-01 14:57:03 -0700

    Create SignInPage class component when its form is submitted it attempts fetch to API sessions#create to confirm sign in, retrieve cookie & redirect to home on successful login otherwise display errors so on successful sign in or App mount, App fetches user's info from API users#current & updates its currentUser state passing it to Navbar as props

--- biddr_client/src/components/App.js
+++ biddr_client/src/components/App.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
 import AuctionShowPage from './AuctionShowPage';
 import AuctionIndexPage from './AuctionIndexPage';
 import NavBar from './NavBar';
+import SignInPage from './SignInPage';
+import { User } from '../requests';
 
-function App() {
-    return (
-        <BrowserRouter>
-            <div>
-                <NavBar />
-                <Switch>
-                    <Route path="/" exact component={WelcomePage} />
-                    <Route path="/auctions/" exact component={AuctionIndexPage} />
-                    <Route path="/auctions/:id" component={AuctionShowPage} />
-                </Switch>
-            </div>
-        </BrowserRouter>
-    );
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            currentUser: null,
+            isLoading: true,
+        }
+        this.getUser = this.getUser.bind(this);
+    }
+
+    getUser(){
+        User.current()
+        .then(data => {
+            if (typeof data.id !== "number") {
+                this.setState({ isLoading: false });
+            } else {
+                this.setState({ currentUser: data, isLoading: false});
+            }
+        });
+    }
+
+    componentDidMount(){
+        this.getUser();
+    }
+
+    render(){
+        const { isLoading, currentUser } = this.state;
+        
+        if (isLoading) {
+            return <div/>
+        }
+
+        return (
+            <BrowserRouter>
+                <div>
+                    <NavBar currentUser={currentUser}/>
+                    <Switch>
+                        <Route path="/" exact component={WelcomePage} />
+                        <Route path="/auctions/" exact component={AuctionIndexPage} />
+                        <Route path="/auctions/:id" component={AuctionShowPage} />
+                        <Route path="/sign_in" render={routeProps => (
+                                <SignInPage onSignIn={this.getUser} {...routeProps}/>
+                            )} 
+                        />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App; 
