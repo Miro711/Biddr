aec4e5aadbf5034b8f4a949e213efd6a1324692c
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-04-01 10:49:23 -0700

    Handle API validation errors in a generic way at Application Controller and refactor Auction & Bid create actions to raise errors upon saving

--- biddr_api/app/controllers/application_controller.rb
+++ biddr_api/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
     private
     def current_user
         if session[:user_id].present?
@@ -10,5 +11,23 @@ class ApplicationController < ActionController::API
         unless current_user.present?
           render json: {status: 401}, status: 401
         end
+    end
+
+    def record_invalid(error)
+        record = error.record # instance of model that failed validation
+
+        errors = record.errors.map do |field, message|
+          {
+            type: error.class.to_s,
+            record_type: record.class.to_s,
+            field: field,
+            message: message
+          }
+        end
+
+        render(
+          json: { status: 422, errors: errors },
+          status: 422
+        )
     end
 end
