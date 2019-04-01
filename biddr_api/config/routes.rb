65bdc901de58955ec84dfc270da0a6de6e28ab26
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-04-01 10:25:00 -0700

    Generate api/v1/sessions controller with create & destroy actions with corresponding routes and add authenticate_user! method in Application Controller to authenticate user

--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do # /api...
+    namespace :v1 do # /api/v1...
+      resource :session, only: [:create, :destroy] # /api/v1/session
+    end
+  end
 end
