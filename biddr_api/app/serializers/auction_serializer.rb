87fadcb21e43c753d6d959061fa88c85dca27287
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-04-01 18:26:49 -0700

    Order bids at Auction show page by bid amount in descending order

--- biddr_api/app/serializers/auction_serializer.rb
+++ biddr_api/app/serializers/auction_serializer.rb
@@ -12,6 +12,10 @@ class AuctionSerializer < ActiveModel::Serializer
   belongs_to(:user, key: :seller)
   has_many(:bids)
 
+  def bids
+    object.bids.order("price desc")
+  end
+
   class BidSerializer < ActiveModel::Serializer
     attributes :id, :price, :created_at, :updated_at, :auction_id
     belongs_to(:user)
